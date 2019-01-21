6d2a2762e2739cd62f7b1c8dab1e23a352fcadb3
Candice <candice.marie.mcdermott@gmail.com>
2019-01-20 19:28:28 -0500

    Adding Comment Option to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
+    has_many :comments
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+    end
 end
