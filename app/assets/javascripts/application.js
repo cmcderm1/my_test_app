b73e5a5b027e348bcabf8893ced3c4a9af045d09
Candice <candice.marie.mcdermott@gmail.com>
2019-01-21 23:08:19 -0500

    Updates to dispaly star rating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
-//= require jquery.raty.js
+
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
@@ -13,7 +13,10 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery3
-//= require_tree .
+//= require_tree
+//= require jquery.raty.js
+//= require jquery_ujs
+
 // Sticky Header
 $(window).scroll(function() {
 
