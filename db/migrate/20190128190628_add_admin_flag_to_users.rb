5f528b38901e5746df72a987cacf9ff338f59584
Candice <candice.marie.mcdermott@gmail.com>
2019-01-28 16:17:41 -0500

    Update 20190128190628_add_admin_flag_to_users.rb

--- db/migrate/20190128190628_add_admin_flag_to_users.rb
+++ db/migrate/20190128190628_add_admin_flag_to_users.rb
@@ -1,4 +1,4 @@
-class AddAdminFlagToUsers < ActiveRecord::Migration
+class AddAdminFlagToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :admin, :boolean, default: false, null: false
   end
