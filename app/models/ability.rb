94624dd9df824db7233bfeb9a4bc85c95c7852cc
Candice <candice.marie.mcdermott@gmail.com>
2019-01-29 15:08:24 -0500

    remove "end" ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,8 +10,6 @@ class Ability
       can :manage, :all
     end
   end
-end
-  
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
