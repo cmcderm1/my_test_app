eb2dbf6c53b0b62de0401746d3452ad90f018fec
Candice <candice.marie.mcdermott@gmail.com>
2019-01-29 15:40:52 -0500

    moved end

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,7 @@ def create
       format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
       format.json { render json: @comment.errors, status: :unprocessable_entity }
     end
+    end
 end
 
 private
@@ -27,5 +28,4 @@ private
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
-end
 end
