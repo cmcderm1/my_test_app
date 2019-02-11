03458aaa2b033713175911d040fb2131b5dd221b
Candice <candice.marie.mcdermott@gmail.com>
2019-02-11 09:31:35 -0500

    6.15 - fixing Ray stars

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,41 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def create
+    @product = Product.find(params[:product_id])
+    @comment = @product.comments.new(comment_params)
+    @comment.user = current_user
+    @user = current_user
+
+    respond_to do |format|
+      if @comment.save
+      #  ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        format.html { redirect_to @product, notice: 'Thanks for your Review!' }
+        format.json { render :show, status: :created, location: @product }
+        format.js
+      else
+        format.html { redirect_to @product, alert: 'You need to add both Star-Rating and a Comment to create a Review!' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def destroy
     @comment = Comment.find(params[:id])
     product = @comment.product
     @comment.destroy
-    redirect_to product
+    respond_to do |format|
+      format.html { redirect_to product, alert: 'Your Review was deleted!'}
+      format.json { head :no_content }
+    end
   end
 
-def create
-  @product = Product.find(params[:product_id])
-  @comment = @product.comments.new(comment_params)
-  @comment.user = current_user
-  @user = current_user
-
-  respond_to do |format|
-    if @comment.save
-      # ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-      ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating      format.html { redirect_to @product, notice: 'Review was created successfully.' }
-      format.json { render :show, status: :created, location: @product }
-      format.js
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
+  private
+  # Using a private method to encapsulate the permissible parameters is
+  # a good pattern since you'll be able to reuse the same permit
+  # list between create and update. Also, you can specialize this method
+  # with per-user checking of permissible attributes.
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
     end
-    end
-end
-
-private
-
-  def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
-  end
 end
