8d0ef0ccc8364ad743d9c48e5cb5364b39f00b69
Candice <candice.marie.mcdermott@gmail.com>
2019-05-07 17:42:49 -0400

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_01_28_190628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
