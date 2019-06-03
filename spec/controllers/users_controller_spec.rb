992a82274c5a57769f8e2024e6b948f28bb4c216
Candice <candice.marie.mcdermott@gmail.com>
2019-06-03 14:25:24 -0400

    spec update and sign-in update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
   let: (:user) = {FactoryBot.create(:user)}
-  let (:user2) = {FactoryBot.create(:user2)}
+  let (:user2) = {FactoryBot.create(:user)}
 
   describe 'GET #show' do
     context 'when a user is logged in' do
       before do
-        sign_in @user
+        sign_in user
       end
 
       it 'loads correct user details' do
-        get :show, params: {id: @user.id}
+        get :show, params: {id: user.id}
         expect(response).to be_ok
-        expect(assigns(:user)).to eq @user
+        expect(assigns(:user)).to eq user
       end
     end
 
       context 'when a user is not logged in' do
         it 'redirects to login' do
-         get :show, params: { id: @user.id }
+         get :show, params: { id: user.id }
          expect(response).to redirect_to (root_path)
         end
       end
