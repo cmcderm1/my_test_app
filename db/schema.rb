6790a39a1fe5111acf5fcde103b7a75bcb3acd65
Candice <candice.marie.mcdermott@gmail.com>
2019-06-11 18:03:35 -0400

    Redis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_015315) do
+ActiveRecord::Schema.define(version: 2019_06_11_202830) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
